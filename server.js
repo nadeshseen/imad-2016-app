9270aadfea9af5638050e3cee48ac4d91a49d325
nadeshseen <nadeshseen@gmail.com>
2017-01-10 15:23:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,12 +12,9 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 var pool = new Pool(config);
 app.get('/test-db', function ( req , res ) {
     pool.query('SELECT * FROM test',function(err,result){
@@ -30,12 +27,9 @@ app.get('/test-db', function ( req , res ) {
     });
     
 });
-
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
